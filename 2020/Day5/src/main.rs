217e64b49ef0999de84a0eb6d25ddef4209e1fd0
Pedro Rodrigues <csixteen@protonmail.com>
2020-12-05 13:22:50 +0100

    Day 5 (second star)

--- 2020/Day5/src/main.rs
+++ 2020/Day5/src/main.rs
@@ -5,7 +5,7 @@
 use std::fs::File;
 use std::io::Read;
 
-fn seat_row(seat: &str) -> i32 {
+fn seat_row(seat: &str) -> usize {
     let (mut lo, mut hi) = (0, 127);
 
     for c in seat.chars() {
@@ -20,7 +20,7 @@ fn seat_row(seat: &str) -> i32 {
     hi
 }
 
-fn seat_col(seat: &str) -> i32 {
+fn seat_col(seat: &str) -> usize {
     let (mut lo, mut hi) = (0, 7);
 
     for c in seat.chars() {
@@ -35,15 +35,27 @@ fn seat_col(seat: &str) -> i32 {
     hi
 }
 
-fn seat_id(seat: &str) -> i32 {
+fn seat_id(seat: &str) -> usize {
     seat_row(seat) * 8 + seat_col(seat)
 }
 
-fn get_max_id(seats: &Vec<&str>) -> i32 {
+fn get_max_id(seats: &Vec<&str>) -> usize {
     seats.iter().fold(0, |acc, seat| {
         let id = seat_id(seat);
         acc.max(id)
     })
+}
+
+fn find_missing_seat(seats: &Vec<&str>) -> usize {
+    let min_id = seats.iter().map(|seat| seat_id(seat)).min().unwrap();
+    let max_id = get_max_id(seats);
+
+    let mut all_seats = vec![false; 1024];
+    for seat in seats.iter() {
+        all_seats[seat_id(seat)] = true;
+    }
+
+    (min_id..max_id).find(|id| !all_seats[*id]).unwrap()
 }
 
 fn main() -> std::io::Result<()> {
@@ -54,6 +66,7 @@ fn main() -> std::io::Result<()> {
     let lines: Vec<&str> = buffer.trim().split("\n").collect();
 
     println!("Day 5 / Part 1: {}", get_max_id(&lines));
+    println!("Day 5 / Part 2: {}", find_missing_seat(&lines));
 
     Ok(())
 }
