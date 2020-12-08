52df60745479ebb474d667f2d4212f0a3311e661
Pedro Rodrigues <csixteen@protonmail.com>
2020-12-08 15:12:47 +0100

    Day 8 (Rust)

---
+++ 2020/Rust/Day8/src/main.rs
@@ -0,0 +1,143 @@
+// https://adventofcode.com/2020/day/8
+
+#![allow(non_snake_case)]
+
+use std::collections::HashSet;
+use std::fs::File;
+use std::io::Read;
+use std::str::FromStr;
+
+#[derive(Clone, Debug, PartialEq)]
+enum Instruction {
+    NOP,
+    JMP,
+    ACC,
+}
+
+#[derive(Default)]
+struct Program {
+    acc: i32,
+    pc: i32,
+    visited: HashSet<i32>,
+    code: Vec<(Instruction, i32)>,
+}
+
+impl Program {
+    fn new(code: &Vec<(Instruction, i32)>) -> Self {
+        Program {
+            code: code.to_vec(),
+            ..Default::default()
+        }
+    }
+
+    fn execute_instruction(&mut self) -> Result<(), i32> {
+        use Instruction::*;
+
+        match self.visited.get(&self.pc) {
+            Some(_) => Err(self.acc),
+            None => {
+                self.visited.insert(self.pc);
+
+                match &self.code[self.pc as usize] {
+                    (NOP, _) => self.pc += 1,
+                    (JMP, n) => self.pc += n,
+                    (ACC, n) => {
+                        self.acc += n;
+                        self.pc += 1;
+                    },
+                };
+
+                Ok(())
+            },
+        }
+    }
+
+    fn execute(&mut self) -> Result<i32, i32> {
+        while (self.pc as usize) < self.code.len() {
+            self.execute_instruction()?;
+        }
+
+        Ok(self.acc)
+    }
+}
+
+fn fix_and_execute(code: &Vec<(Instruction, i32)>) -> i32 {
+    use Instruction::*;
+
+    for i in 0..code.len() {
+        if code[i].0 == ACC { continue; }
+
+        let mut new_code = code.clone();
+        new_code[i] = (
+            if code[i].0 == NOP { JMP } else { NOP },
+            code[i].1,
+        );
+
+        let mut p = Program::new(&new_code);
+        if let Ok(n) = p.execute() {
+            return n;
+        }
+    }
+
+    unreachable!();
+}
+
+fn main() -> std::io::Result<()> {
+    let mut buffer = String::new();
+    let mut file = File::open("data/input.txt")?;
+
+    file.read_to_string(&mut buffer).unwrap();
+    let code: Vec<(Instruction, i32)> = buffer
+        .trim()
+        .split("\n")
+        .map(|line| {
+            let s: Vec<&str> = line.split(" ").collect();
+            let op = match s[0] {
+                "nop" => Instruction::NOP,
+                "jmp" => Instruction::JMP,
+                "acc" => Instruction::ACC,
+                _ => panic!("Unknown instruction"),
+            };
+
+            (op, i32::from_str(s[1]).unwrap())
+        })
+        .collect();
+
+    let mut p = Program::new(&code);
+
+    println!("Day 8 / Part 1: {:?}", p.execute());
+    println!("Day 8 / Part 2: {}", fix_and_execute(&code));
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const CODE: [(Instruction, i32); 9] = [
+        (Instruction::NOP, 0),
+        (Instruction::ACC, 1),
+        (Instruction::JMP, 4),
+        (Instruction::ACC, 3),
+        (Instruction::JMP, -3),
+        (Instruction::ACC, -99),
+        (Instruction::ACC, 1),
+        (Instruction::JMP, -4),
+        (Instruction::ACC, 6),
+    ];
+
+    #[test]
+    fn test_execute() {
+        let mut p = Program::new(&CODE.to_vec());
+
+        assert_eq!(Err(5), p.execute());
+    }
+
+    #[test]
+    fn test_fix_and_execute() {
+        let mut p = Program::new(&CODE.to_vec());
+
+        assert_eq!(8, fix_and_execute(&CODE.to_vec()));
+    }
+}
