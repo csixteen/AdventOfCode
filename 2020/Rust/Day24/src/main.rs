32c36b2587fcd2113c807618e09fb7c188e0f352
Pedro Rodrigues <csixteen@protonmail.com>
2020-12-27 13:53:39 +0100

    Day 24 - Part 2 (Rust)

--- 2020/Rust/Day24/src/main.rs
+++ 2020/Rust/Day24/src/main.rs
@@ -24,10 +24,12 @@
 
 #![allow(non_snake_case)]
 
-use std::collections::HashMap;
+use std::collections::HashSet;
 
 use aoc::fs::get_file_contents;
 
+
+type Tiles = HashSet<(i32, i32)>;
 
 fn process_steps(line: &String) -> (i32, i32) {
     let len = line.len();
@@ -57,16 +59,14 @@ fn process_steps(line: &String) -> (i32, i32) {
     (x, y)
 }
 
-fn black_tiles(lines: &Vec<String>) -> usize {
-    let tiles: HashMap<(i32, i32), i32> = lines
+fn black_tiles(lines: &Vec<String>) -> Tiles {
+    lines
         .iter()
-        .fold(HashMap::new(), |mut acc, line| {
+        .fold(HashSet::new(), |mut acc, line| {
             let (x, y) = process_steps(line);
-            acc.entry((x, y)).and_modify(|e| { *e *= -1 }).or_insert(1);
+            if !acc.remove(&(x, y)) { acc.insert((x, y)); }
             acc
-        });
-
-    tiles.values().filter(|&v| *v == 1).count()
+        })
 }
 
 fn neighbours(x: i32, y: i32) -> Vec<(i32, i32)> {
@@ -80,32 +80,45 @@ fn neighbours(x: i32, y: i32) -> Vec<(i32, i32)> {
     ]
 }
 
-fn process_day(tiles: HashMap<(i32, i32), i32>) -> HashMap<(i32, i32), i32> {
-    let mut ret = HashMap::new();
+fn process_day(tiles: &Tiles) -> Tiles {
+    let mut ret: Tiles = HashSet::new();
+    let to_process: Tiles = tiles
+        .iter()
+        .fold(HashSet::new(), |mut acc, &(x, y)| {
+            acc.insert((x, y));
+            neighbours(x, y).iter().for_each(|&(x1, y1)| { acc.insert((x1, y1)); });
+            acc
+        });
+
+    for &(x, y) in to_process.iter() {
+        let c = neighbours(x, y)
+            .iter()
+            .filter(|&(x1, y1)| tiles.contains(&(*x1, *y1)))
+            .count();
+
+        match tiles.contains(&(x, y)) {
+            true => if c > 0 && c < 3 { ret.insert((x, y)); },
+            false => if c == 2 { ret.insert((x, y)); },
+        }
+    }
 
     ret
 }
 
 fn art_exhibit(lines: &Vec<String>, days: usize) -> usize {
-    let mut tiles: HashMap<(i32, i32), i32> = lines
-        .iter()
-        .fold(HashMap::new(), |mut acc, line| {
-            let (x, y) = process_steps(line);
-            acc.entry((x, y)).and_modify(|e| { *e *= -1 }).or_insert(1);
-            acc
-        });
+    let mut tiles = black_tiles(lines);
 
     for _ in 0..days {
-        tiles = process_day(tiles);
+        tiles = process_day(&tiles);
     }
 
-    tiles.values().filter(|&v| *v == 1).count()
+    tiles.len()
 }
 
 fn main() -> std::io::Result<()> {
     let lines = get_file_contents("data/input.txt")?;
 
-    println!("Day 24 / Part 1: {}", black_tiles(&lines));
+    println!("Day 24 / Part 1: {}", black_tiles(&lines).len());
     println!("Day 24 / Part 2: {}", art_exhibit(&lines, 100));
 
     Ok(())
@@ -148,39 +161,39 @@ mod tests {
                     "neswnwewnwnwseenwseesewsenwsweewe".to_string(),
                     "wseweeenwnesenwwwswnew".to_string(),
                 ]
-            ),
+            ).len(),
         );
     }
 
-    //#[test]
-    //fn test_art_exhibit() {
-    //    assert_eq!(
-    //        15,
-    //        art_exhibit(
-    //            &vec![
-    //                "sesenwnenenewseeswwswswwnenewsewsw".to_string(),
-    //                "neeenesenwnwwswnenewnwwsewnenwseswesw".to_string(),
-    //                "seswneswswsenwwnwse".to_string(),
-    //                "nwnwneseeswswnenewneswwnewseswneseene".to_string(),
-    //                "swweswneswnenwsewnwneneseenw".to_string(),
-    //                "eesenwseswswnenwswnwnwsewwnwsene".to_string(),
-    //                "sewnenenenesenwsewnenwwwse".to_string(),
-    //                "wenwwweseeeweswwwnwwe".to_string(),
-    //                "wsweesenenewnwwnwsenewsenwwsesesenwne".to_string(),
-    //                "neeswseenwwswnwswswnw".to_string(),
-    //                "nenwswwsewswnenenewsenwsenwnesesenew".to_string(),
-    //                "enewnwewneswsewnwswenweswnenwsenwsw".to_string(),
-    //                "sweneswneswneneenwnewenewwneswswnese".to_string(),
-    //                "swwesenesewenwneswnwwneseswwne".to_string(),
-    //                "enesenwswwswneneswsenwnewswseenwsese".to_string(),
-    //                "wnwnesenesenenwwnenwsewesewsesesew".to_string(),
-    //                "nenewswnwewswnenesenwnesewesw".to_string(),
-    //                "eneswnwswnwsenenwnwnwwseeswneewsenese".to_string(),
-    //                "neswnwewnwnwseenwseesewsenwsweewe".to_string(),
-    //                "wseweeenwnesenwwwswnew".to_string(),
-    //            ],
-    //            1,
-    //        ),
-    //    );
-    //}
+    #[test]
+    fn test_art_exhibit() {
+        assert_eq!(
+            2208,
+            art_exhibit(
+                &vec![
+                    "sesenwnenenewseeswwswswwnenewsewsw".to_string(),
+                    "neeenesenwnwwswnenewnwwsewnenwseswesw".to_string(),
+                    "seswneswswsenwwnwse".to_string(),
+                    "nwnwneseeswswnenewneswwnewseswneseene".to_string(),
+                    "swweswneswnenwsewnwneneseenw".to_string(),
+                    "eesenwseswswnenwswnwnwsewwnwsene".to_string(),
+                    "sewnenenenesenwsewnenwwwse".to_string(),
+                    "wenwwweseeeweswwwnwwe".to_string(),
+                    "wsweesenenewnwwnwsenewsenwwsesesenwne".to_string(),
+                    "neeswseenwwswnwswswnw".to_string(),
+                    "nenwswwsewswnenenewsenwsenwnesesenew".to_string(),
+                    "enewnwewneswsewnwswenweswnenwsenwsw".to_string(),
+                    "sweneswneswneneenwnewenewwneswswnese".to_string(),
+                    "swwesenesewenwneswnwwneseswwne".to_string(),
+                    "enesenwswwswneneswsenwnewswseenwsese".to_string(),
+                    "wnwnesenesenenwwnenwsewesewsesesew".to_string(),
+                    "nenewswnwewswnenesenwnesewesw".to_string(),
+                    "eneswnwswnwsenenwnwnwwseeswneewsenese".to_string(),
+                    "neswnwewnwnwseenwseesewsenwsweewe".to_string(),
+                    "wseweeenwnesenwwwswnew".to_string(),
+                ],
+                100,
+            ),
+        );
+    }
 }
