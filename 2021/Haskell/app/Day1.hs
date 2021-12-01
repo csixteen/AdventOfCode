4477311785d79f5e234a1ce68fd609a5b9f846ff
Pedro Rodrigues <csixteen@protonmail.com>
2021-12-01 20:02:56 +0100

    Day 1 - refactoring

--- 2021/Haskell/app/Day1.hs
+++ 2021/Haskell/app/Day1.hs
@@ -1,16 +1,10 @@
 module Day1 where
 
 
-import System.IO
-import Text.Read
-
-
 solve :: FilePath -> Int -> IO Integer
 solve fileName n =
   do xs <- listFromFile fileName
-     case xs of
-       Nothing  -> error "Bad input"
-       Just xs' -> return (larger xs' n)
+     return (larger xs n)
 
 
 -- Takes a list of Integers and the size of a sliding winndow and
@@ -27,36 +21,8 @@ larger xs n | length xs < n = error "Bad list!"
                                            in l_than (tail xs') new_prev new_acc
 
 
--- -----------------------------------------------------------
-
--- Helpers for reading input from a file as a list of Integers
-
-entryFromHandle :: Handle -> IO (Maybe Integer)
-entryFromHandle fh =
-  do n <- hGetLine fh
-     case readMaybe n :: Maybe Integer of
-       Nothing -> return Nothing
-       Just x  -> return (Just x)
-
-
-listFromHandle :: Handle -> IO (Maybe [Integer])
-listFromHandle fh =
-  do ended <- hIsEOF fh
-     if ended then return (Just [])
-     else
-       do x <- entryFromHandle fh
-          case x of
-            Nothing -> return Nothing
-            Just x' ->
-              do xs <- listFromHandle fh
-                 case xs of
-                   Nothing  -> return Nothing
-                   Just xs' -> return (Just (x' : xs'))
-
-
-listFromFile :: FilePath -> IO (Maybe [Integer])
+listFromFile :: FilePath -> IO [Integer]
 listFromFile fileName =
-  do fh <- openFile fileName ReadMode
-     xs <- listFromHandle fh
-     hClose fh
-     return xs
+  do contents <- readFile fileName
+     let numbers = map (\x -> read x :: Integer) (lines contents)
+     return numbers
