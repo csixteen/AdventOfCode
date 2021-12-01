7ee214d44bbcacb15e210d9b3dc7549a09327b7c
Pedro Rodrigues <csixteen@protonmail.com>
2021-12-01 11:46:57 +0100

    2021 - Day 1 (part 2)

--- 2021/Haskell/app/Day1.hs
+++ 2021/Haskell/app/Day1.hs
@@ -5,14 +5,16 @@ import System.IO
 import Text.Read
 
 
-solve1 :: FilePath -> IO Integer
-solve1 fileName =
+solve :: FilePath -> ([Integer] -> Integer) -> IO Integer
+solve fileName f =
   do xs <- listFromFile fileName
      case xs of
        Nothing  -> error "Bad input"
-       Just xs' -> return (larger_than xs')
+       Just xs' -> return (f xs')
 
 
+-- Solves part 1, by simply counting the number of entries in the list
+-- that are larger than the previous one.
 larger_than :: [Integer] -> Integer
 larger_than [] = error "Empty list!"
 larger_than [_] = 0
@@ -23,6 +25,24 @@ larger_than xs = l_than xs (head xs) 0
           let new_acc = if y > prev then acc + 1 else acc
           in l_than ys y new_acc
 
+
+-- Solves part 2, by counting the number of sliding windows of length 3 that
+-- are larger than the previous sliding window. This is very generic (it wouldn't
+-- work for a N-size sliding window.
+larger_than_sliding :: [Integer] -> Integer
+larger_than_sliding xs@(_:_:_:_) = l_than (drop 1 xs) (sum $ (take 3 xs)) 0
+  where
+    l_than xs' prev acc | l < 3 = acc
+                        | otherwise = let new_prev = sum $ take 3 xs'
+                                          new_acc  = if new_prev > prev then acc + 1 else acc
+                                      in l_than (drop 1 xs') new_prev new_acc
+                        where l = length xs'
+larger_than_sliding _ = error "Bad list!"
+
+
+-- -----------------------------------------------------------
+
+-- Helpers for reading input from a file as a list of Integers
 
 entryFromHandle :: Handle -> IO (Maybe Integer)
 entryFromHandle fh =
--- 2021/Haskell/app/Main.hs
+++ 2021/Haskell/app/Main.hs
@@ -1,4 +1,10 @@
 module Main where
 
+import Day1
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main =
+  do x <- Day1.solve "data/day1_input.txt" Day1.larger_than
+     putStrLn $ "Day1 (part 1): " ++ (show x)
+     x <- Day1.solve "data/day1_input.txt" Day1.larger_than_sliding
+     putStrLn $ "Day1 (part 2): " ++ (show x)
