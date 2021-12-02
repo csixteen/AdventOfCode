946ac71024d032f6483a45ce293fd9fbef5e110c
Pedro Rodrigues <csixteen@protonmail.com>
2021-12-02 09:03:09 +0100

    Day 2 (part 1)

---
+++ 2021/Haskell/app/Day2.hs
@@ -0,0 +1,51 @@
+module Day2 where
+
+import Data.List
+
+type Vec = (Int, Int)
+
+data Dir = Up Int
+         | Down Int
+         | Forward Int
+  deriving Show
+
+
+solve :: FilePath -> IO Int
+solve fileName =
+  do directions <- directionsFromFile fileName
+     let (h, d) = navigate (0, 0) directions
+     return (h*d)
+
+
+-- Take a list of directions and an initial position
+-- and returns the final position after having navigated
+-- according to the directions.
+navigate :: Vec -> [Dir] -> Vec
+navigate pos dirs = foldl' move pos dirs
+
+
+-- Takes a Vec that represents a position and a direction
+-- and returns a new position.
+move :: Vec -> Dir -> Vec
+move (h, d) dir =
+  case dir of
+    Up n      -> (h, d-n)
+    Down n    -> (h, d+n)
+    Forward n -> (h+n, d)
+
+
+
+directionsFromFile :: FilePath -> IO [Dir]
+directionsFromFile fileName =
+  do contents <- readFile fileName
+     let dirs = map toDir $ fmap words $ lines contents
+     return dirs
+
+
+toDir :: [String] -> Dir
+toDir [d, n] =
+  case d of
+    "forward" -> Forward (read n :: Int)
+    "up"      -> Up (read n :: Int)
+    "down"    -> Down (read n :: Int)
+    _         -> error "Unknown direction"
--- 2021/Haskell/app/Main.hs
+++ 2021/Haskell/app/Main.hs
@@ -1,6 +1,7 @@
 module Main where
 
 import Day1
+import Day2
 
 main :: IO ()
 main =
@@ -8,3 +9,5 @@ main =
      putStrLn $ "Day1 (part 1): " ++ (show x)
      x <- Day1.solve "data/day1_input.txt" 3
      putStrLn $ "Day1 (part 2): " ++ (show x)
+     x <- Day2.solve "data/day2_input.txt"
+     putStrLn $ "Day2 (part 1): " ++ (show x)
