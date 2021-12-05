251bcfb20e3ffe2cd26caf0056857c0b488475ed
Pedro Rodrigues <csixteen@protonmail.com>
2021-12-05 14:56:45 +0100

    Day 4 - part 2

--- 2021/Haskell/app/Day4.hs
+++ 2021/Haskell/app/Day4.hs
@@ -19,6 +19,7 @@ solve fileName =
      return (n1 * sumBoard b1, n2 * sumBoard b2)
 
 
+-- Returns the first winning board and the number drawn.
 play :: [Int] -> [Board] -> (Int, Board)
 play (n:ns) bs =
   let bs' = map (update n) bs
@@ -27,6 +28,7 @@ play (n:ns) bs =
     Just b  -> (n, b)
 
 
+-- Returns the last winning board and the number drawn.
 lastBoard :: [Int] -> [Board] -> (Int, Board)
 lastBoard ns bs = (lastN, head winners)
   where
@@ -34,6 +36,8 @@ lastBoard ns bs = (lastN, head winners)
     (_, winners, lastN) = fromJust $ find (\(l, w, n) -> null l) bs'
 
 
+-- Creates a full history with all the losing and winning boards and
+-- the corresponding drawn number.
 history :: [Int] -> [Board] -> [([Board], [Board], Int)]
 history ns bs = scanl part (bs, [], -1) ns
   where
@@ -42,6 +46,9 @@ history ns bs = scanl part (bs, [], -1) ns
       where (winners, losers) = partition winner $ map (update n) bs
 
 
+-- Given a number and a board, it returns a board where the cell with
+-- that number is marked. If the number doesn't occur in the board, it
+-- returns the same board.
 update :: Int -> Board -> Board
 update n b =
   case index n b of
@@ -49,6 +56,15 @@ update n b =
     Just (row, col) -> mark row col b
 
 
+-- Marks a specific cell in a board, given a row and a column, and returns
+-- the updated board.
+mark :: Int -> Int -> Board -> Board
+mark r c b = (take r b) ++ ((mark' c $ b !! r) : (drop (r+1) b))
+  where mark' col row = (take col row) ++ ((fst $ row !! col, True) : (drop (col+1) row))
+
+
+-- Checks whether there is winner board in a list of boards and returns
+-- a Maybe value with the winner, if there is one.
 existsWinner :: [Board] -> Maybe Board
 existsWinner []     = Nothing
 existsWinner (b:bs) = case winner b of
@@ -56,16 +72,20 @@ existsWinner (b:bs) = case winner b of
                         False -> existsWinner bs
 
 
+-- Checks whether a board is a winner or not.
 winner :: Board -> Bool
 winner b = winner' b || winner' (transpose b)
   where winner' = any winRow
         winRow  = all snd
 
 
+-- Returns the sum of all the unmarked cells in a board.
 sumBoard :: Board -> Int
 sumBoard = sum . map (foldl' (\acc x -> acc + fst x) 0 . filter (not . snd))
 
 
+-- Returns a Maybe (Int, Int) representing the row and column where
+-- a number occurs in a board, shoud that be the case.
 index :: Int -> Board -> Maybe (Int, Int)
 index n b = findInBoard 0 b
   where findInBoard _ [] = Nothing
@@ -75,6 +95,8 @@ index n b = findInBoard 0 b
             Just col -> Just (row, col)
 
 
+-- Returns a Maybe Int with the column where a number occurs in a row,
+-- should that be the case.
 findPos :: Int -> [Cell] -> Maybe Int
 findPos n row =
   let indices = map fst $ filter ((n==) . fst . snd) $ zip [0..] row
@@ -83,9 +105,8 @@ findPos n row =
     xs -> Just (head xs)
 
 
-mark :: Int -> Int -> Board -> Board
-mark r c b = (take r b) ++ ((mark' c $ b !! r) : (drop (r+1) b))
-  where mark' col row = (take col row) ++ ((fst $ row !! col, True) : (drop (col+1) row))
+-- ----------------------------------------
+--           Scaffolding
 
 
 bingoFromList :: [String] -> [Board]
