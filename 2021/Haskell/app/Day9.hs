752220a9723e47f8f09bd2ad34fdfce10810fb84
Pedro Rodrigues <csixteen@protonmail.com>
2021-12-10 23:01:59 +0100

    Day 9 - part 2

--- 2021/Haskell/app/Day9.hs
+++ 2021/Haskell/app/Day9.hs
@@ -1,23 +1,48 @@
 module Day9 where
 
 import Data.Char
-import Debug.Trace
+import Data.List
+import qualified Data.Set as S
 
 
 solve :: FilePath -> IO (Int, Int)
 solve fileName =
   do contents <- readFile fileName
      let
-       hm    = heatmap $ lines contents
-       part1 = sum $ lowPoints hm
-     return (part1,1)
+       hm      = heatmap $ lines contents
+       part1   = sum $ map ((+1) . (point hm)) $ lowPoints hm
+       [a,b,c] = take 3 $ sortBy (\a b -> compare b a) $ map length $ basins hm
+     return (part1,a*b*c)
 
 
 type Heatmap = [[Int]]
+type Basin   = [(Int,Int)]
+type PairSet = S.Set (Int,Int)
 
 
-lowPoints :: Heatmap -> [Int]
-lowPoints hm = map ((+1) . (point hm)) $ filter (isLowPoint hm) points
+basins :: Heatmap -> [Basin]
+basins hm = map (basin hm) $ lowPoints hm
+
+
+basin :: Heatmap -> (Int,Int) -> Basin
+basin hm p = S.toList acc
+  where
+    (acc, _) = basin' hm p S.empty S.empty
+
+
+basin' :: Heatmap -> (Int,Int) -> PairSet -> PairSet -> (PairSet, PairSet)
+basin' hm p visited acc
+  | (S.member p visited) || (9 == point hm p) = (acc, visited)
+  | otherwise = foldr addNeighbor (S.insert p acc, S.insert p visited) $ neighbors p hm
+  where
+    addNeighbor :: (Int,Int) -> (PairSet, PairSet) -> (PairSet, PairSet)
+    addNeighbor neighbor (acc', visited') =
+      let (acc'', visited'') = basin' hm neighbor visited' acc'
+      in  (S.union acc' acc'', S.union visited' visited'')
+
+
+lowPoints :: Heatmap -> [(Int,Int)]
+lowPoints hm = filter (isLowPoint hm) points
   where
     points         = [(x,y) | x <- [0..mRows], y <- [0..mCols]]
     (mRows, mCols) = ((length hm)-1, (length $ head hm)-1)
