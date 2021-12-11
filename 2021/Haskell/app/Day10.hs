0b721b9ffc34559a88175afe9376d87ee0d32e42
Pedro Rodrigues <csixteen@protonmail.com>
2021-12-11 21:44:45 +0100

    Day 10 - part 2

--- 2021/Haskell/app/Day10.hs
+++ 2021/Haskell/app/Day10.hs
@@ -1,5 +1,6 @@
 module Day10 where
 
+import Data.List
 import Data.Maybe
 
 
@@ -8,7 +9,9 @@ solve fileName =
   do contents <- readFile fileName
      let
        part1  = sum $ map (fromJust . fst) $ filter (isJust . fst) $ map validate $ lines contents
-     return (part1,1)
+       scores = map (complete . snd) $ filter (isNothing . fst) $ map validate $ lines contents
+       part2  = (sort scores) !! ((length scores) `div` 2)
+     return (part1,part2)
 
 
 matches :: [(Char,Char)]
@@ -26,6 +29,27 @@ points :: Char -> Maybe Int
 points c = lookup c pts
   where
     pts = [(')', 3), (']', 57), ('}', 1197), ('>', 25137)]
+
+
+points2 :: Char -> Maybe Int
+points2 c = lookup c pts
+  where
+    pts = [(')', 1), (']', 2), ('}', 3), ('>', 4)]
+
+
+complete :: String -> Int
+complete chunks = foldl' score 0 res
+  where
+    score acc c = acc*5 + (fromJust $ points2 c)
+    res = complete' chunks []
+
+
+complete' :: String -> [Char] -> [Char]
+complete' [] stack = map findMatch stack
+complete' (x:xs) s =
+  case isOpening x of
+    True  -> complete' xs (x:s)
+    False -> complete' xs (tail s)
 
 
 validate :: String -> (Maybe Int, String)
--- 2021/Haskell/app/Main.hs
+++ 2021/Haskell/app/Main.hs
@@ -5,6 +5,6 @@ import Day10
 
 main :: IO ()
 main =
-  do (x, y) <- Day10.solve "data/day10_sample.txt"
+  do (x, y) <- Day10.solve "data/day10_input.txt"
      putStrLn $ "Day10 (part 1): " ++ (show x)
      putStrLn $ "Day10 (part 2): " ++ (show y)
