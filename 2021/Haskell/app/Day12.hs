cae5901f27ce9b3af3a6444ab74ce769a66be4a7
Pedro Rodrigues <csixteen@protonmail.com>
2021-12-14 11:24:58 +0100

    Day 12 - part 2

--- 2021/Haskell/app/Day12.hs
+++ 2021/Haskell/app/Day12.hs
@@ -4,7 +4,6 @@ import Data.Char
 import Data.List
 import Data.List.Split
 import qualified Data.Map.Strict as M
-import qualified Data.Set as S
 
 
 solve :: FilePath -> IO (Int,Int)
@@ -13,14 +12,14 @@ solve fileName =
      let
        g = graph $ lines contents
        allPaths = paths g "start" "end"
-       part1 = length allPaths
-     return (part1,1)
+       part2 = length allPaths
+     return (1,part2)
 
 
 type Graph   = M.Map Vertex [Vertex]
 type Vertex  = String
 type Path    = [Vertex]
-type Visited = S.Set Vertex
+type Visited = M.Map Vertex Int
 
 
 graph :: [String] -> Graph
@@ -37,23 +36,37 @@ addEdge g s t = M.insertWith (++) t [s] g'
 
 
 paths :: Graph -> Vertex -> Vertex -> [Path]
-paths g s t = paths' g s t S.empty
+paths g s t = paths' g s t M.empty
 
 
 paths' :: Graph -> Vertex -> Vertex -> Visited -> [Path]
 paths' g s t vis
-  | s == t = [[s]]
-  | otherwise =
-    case filter (\t' -> not $ S.member t' vis) $ g M.! s of
+  | s == t            = [[t]]
+  | not $ valid vis s = []
+  | otherwise         =
+    case filter (valid vis) $ g M.! s of
       [] -> []
-      xs ->
-        let
-          vis' = visit vis s
-          pss = foldl' (\acc s' -> acc ++ (paths' g s' t vis')) [] xs
-        in
-          fmap (s:) pss
+      xs -> let vis' = visit vis s
+                pss = foldl' (\acc s' -> acc ++ (paths' g s' t vis')) [] xs
+            in fmap (s:) pss
+
+
+special :: Vertex -> Bool
+special v = v `elem` ["start", "end"]
+
+
+bigCave :: Vertex -> Bool
+bigCave = all isUpper
+
+
+valid :: Visited -> Vertex -> Bool
+valid vis v | bigCave v                         = True
+            | not $ M.member v vis              = True
+            | special v                         = False
+            | (length $ M.filter (>1) vis) == 0 = True
+            | otherwise                         = False
 
 
 visit :: Visited -> Vertex -> Visited
-visit vis v | all isUpper v = vis
-            | otherwise     = S.insert v vis
+visit vis v | bigCave v = vis
+            | otherwise = M.insertWith (+) v 1 vis
