f0ea07bd2767872952a6770ef77d60e6e1f24172
Pedro Rodrigues <csixteen@protonmail.com>
2021-12-22 17:48:52 +0100

    Day 18 - part 2

--- 2021/Haskell/app/Day18.hs
+++ 2021/Haskell/app/Day18.hs
@@ -2,15 +2,17 @@ module Day18 where
 
 import Prelude hiding (lines)
 
+import Control.Applicative
 import Data.Either
 import Data.List hiding (lines)
-import Data.Text hiding (foldl1')
+import Data.Maybe
+import Data.Text hiding (foldl1',maximum)
 import Relude.File (readFileText)
 import Relude.Functor.Fmap
 import Relude.String.Conversion hiding (show)
 import Text.Parsec.Char
 import Text.Parsec.Combinator
-import Text.Parsec.Prim
+import Text.Parsec.Prim hiding ((<|>))
 import Text.Parsec.Text
 
 
@@ -18,8 +20,9 @@ solve :: IO (Int,Int)
 solve =
   do numbers <- parseSFNumber <<$>> lines <$> readFileText "data/day18_input.txt"
      let
-       part1 = magnitude $ foldl1' addNumbers numbers 
-     return (part1,1)
+       part1 = magnitude $ foldl1' addNumbers numbers
+       part2 = maximum $ magnitudes numbers
+     return (part1,part2)
 
 
 -- ---------------------------
@@ -37,6 +40,10 @@ data SFNumber = Num Int
 -- --------------------
 
 
+magnitudes :: [SFNumber] -> [Int]
+magnitudes ns = [(magnitude . uncurry addNumbers) (a,b) | a <- ns, b <- ns, a /= b]
+
+
 magnitude :: SFNumber -> Int
 magnitude (Num i) = i
 magnitude (Pair a b) = 3 * a' + 2 * b'
@@ -46,7 +53,52 @@ magnitude (Pair a b) = 3 * a' + 2 * b'
 
 
 addNumbers :: SFNumber -> SFNumber -> SFNumber
-addNumbers = undefined
+addNumbers a b = reduce $ Pair a b
+
+
+reduce :: SFNumber -> SFNumber
+reduce n = maybe n reduce (explodeNumber n <|> splitNumber n)
+
+
+explodeNumber :: SFNumber -> Maybe SFNumber
+explodeNumber n = fst <$> explode 0 n 
+
+
+explode :: Int -> SFNumber -> Maybe (SFNumber, (Int,Int))
+explode _ (Num _) = Nothing
+explode depth (Pair (Num l) (Num r)) | depth >= 4 = Just (Num 0, (l, r))
+explode depth (Pair left right)  = (explodeLeft right <$> left') <|> (explodeRight left <$> right')
+  where
+    left'  = explode (depth + 1) left
+    right' = explode (depth + 1) right
+
+
+explodeLeft :: SFNumber -> (SFNumber, (Int,Int))-> (SFNumber, (Int,Int))
+explodeLeft rightmost (num, (left, right)) = (Pair num (addLeft right rightmost), (left,0))
+  where
+    addLeft :: Int -> SFNumber -> SFNumber
+    addLeft n (Num x)    = Num (x+n)
+    addLeft n (Pair l r) = Pair (addLeft n l) r
+
+
+explodeRight :: SFNumber -> (SFNumber, (Int,Int)) -> (SFNumber, (Int,Int))
+explodeRight leftmost (num, (left, right)) = (Pair (addRight left leftmost) num, (0,right))
+  where
+    addRight :: Int -> SFNumber -> SFNumber
+    addRight n (Num x)    = Num (x+n)
+    addRight n (Pair l r) = Pair l (addRight n r)
+
+
+splitNumber :: SFNumber -> Maybe SFNumber
+splitNumber (Num n)
+  | n < 10    = Nothing
+  | otherwise = Just $ Pair (Num q) (Num $ q + r)
+  where
+    (q,r) = n `divMod` 2
+splitNumber (Pair left right) = left' <|> right'
+  where
+    left'  = flip Pair right <$> splitNumber left
+    right' = Pair left <$> splitNumber right 
 
 
 -- ---------------------
