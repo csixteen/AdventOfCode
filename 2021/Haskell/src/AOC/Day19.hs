94a12bb968449fb11be5f633bc2904e33de4f729
Pedro Rodrigues <csixteen@protonmail.com>
2021-12-29 13:20:19 +0100

    Day 19 (WIP)

--- 2021/Haskell/src/AOC/Day19.hs
+++ 2021/Haskell/src/AOC/Day19.hs
@@ -1,19 +1,32 @@
 module AOC.Day19 where
 
+import Data.Either
 import Data.Monoid
-import Data.MultiSet as M
+import Data.Text
 
+import Data.MultiSet as M
 import Linear (V3(..), (^+^), (^-^))
+import Relude.File (readFileText)
+import Text.Parsec.Char
+import Text.Parsec.Combinator
+import Text.Parsec.Prim
+import Text.Parsec.Text
 
 
 solve :: IO (Int,Int)
 solve =
-  do contents <- readFile "data/day19_input.txt"
+  do scanners <- parseScanners <$> readFileText "data/day19_input.txt"
      return (1,1)
+
+
+-- -----------------------------------------
+--       Type declarations and helpers
+-- -----------------------------------------
 
 
 type Point = V3 Int
 type Transform = Endo Point
+
 
 data Scanner  = Scanner
   { scannerId :: Int
@@ -26,3 +39,61 @@ data Scanner  = Scanner
 
 instance Show Transform where
   show c = show $ appEndo c (V3 0 0 0)
+
+
+nullT = Endo id
+
+
+-- -----------------------------
+--          Parsers
+-- -----------------------------
+
+parseScanners :: Text -> [Scanner]
+parseScanners = fromRight (error "Parsing error") . parse pScanners ""
+
+
+pScanners :: Parser [Scanner]
+pScanners = pScanner `sepBy` many1 endOfLine
+
+
+pScanner :: Parser Scanner
+pScanner = mkScanner <$> pScannerID <*> pBeacons
+  where
+    mkScanner sId beacons = Scanner { scannerId      = sId
+                                    , beacons        = beacons
+                                    , transformation = nullT
+                                    , signature      = sign beacons
+                                    }
+
+
+pScannerID :: Parser Int
+pScannerID =
+  do
+    string "--- scanner "
+    sid <- many1 digit
+    string " ---"
+    return . readInt $ sid
+
+
+pBeacons :: Parser [Point]
+pBeacons = pBeacon `sepBy` endOfLine
+
+
+pBeacon :: Parser Point
+pBeacon = V3 <$> pNumber
+             <*> (char ',' >> pNumber)
+             <*> (char ',' >> pNumber)
+
+
+pNumber :: Parser Int
+pNumber =
+  do
+    digits <- many1 digit
+    return . readInt $ digits
+
+
+sign = undefined
+
+
+readInt :: String -> Int
+readInt = read
