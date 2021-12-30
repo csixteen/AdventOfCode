0635d80117b7c2082896d86477a710e6de42e839
Pedro Rodrigues <csixteen@protonmail.com>
2021-12-30 08:09:12 +0100

    Day 19 (WIP)

--- 2021/Haskell/src/AOC/Day19.hs
+++ 2021/Haskell/src/AOC/Day19.hs
@@ -5,7 +5,7 @@ import Data.Monoid
 import Data.Text
 
 import Data.MultiSet as M
-import Linear (V3(..), (^+^), (^-^))
+import Linear (Additive, V3(..), (^+^), (^-^))
 import Relude.File (readFileText)
 import Text.Parsec.Char
 import Text.Parsec.Combinator
@@ -24,16 +24,17 @@ solve =
 -- -----------------------------------------
 
 
-type Point = V3 Int
-type Transform = Endo Point
+type Point  = V3 Int
+type Rotate = Endo Point
 
 
 data Scanner  = Scanner
   { scannerId :: Int
   , beacons   :: [Point]
-  , transformation :: Endo Point
+  , rotation  :: Rotate
   , signature :: M.MultiSet Int
   }
+  deriving (Show)
 
 
 dist :: Point -> Point -> Int
@@ -52,18 +53,34 @@ couldMatch s1 s2 = s >= (12 * 11) `div` 2
     s = M.size $ M.intersection (signature s1) (signature s2)
 
 
-{-
-   * Rotations *
+-- -------------------------------
+--      Rotations and helpers
+-- -------------------------------
 
+
+{-
 Reference: https://hackage.haskell.org/package/base-4.16.0.0/docs/Data-Monoid.html#t:Endo
 This allows us to apply a single rotation or a composition of rotations to a Vector:
  appEndo rX (V3 1 1 1) == V3 1 (-1) 1
  appEndo (rX <> rZ) (V3 1 1 2) == V3 (-1) (-2) 1
 -}
+nullT,rX,rY,rZ :: Endo (V3 Int)
 nullT = Endo id
 rX = Endo \(V3 x y z) -> V3 x (-z) y
 rY = Endo \(V3 x y z) -> V3 z y (-x)
 rZ = Endo \(V3 x y z) -> V3 (-y) x z
+
+
+-- Returns 24 compositions of rotations in the 3 axis
+rotations :: [Rotate]
+rotations = [x <> y | x <- xs, y <- ys]
+  where
+    xs = [nullT, rY, rY <> rY, rY <> rY <> rY, rZ, rZ <> rZ <> rZ]
+    ys = [nullT, rX, rX <> rX, rX <> rX <> rX]
+
+
+rotate :: (Additive f, Num a) => f a -> Endo (f a)
+rotate v = Endo (v ^+^)
 
 
 -- -----------------------------
@@ -82,10 +99,10 @@ pScanner :: Parser Scanner
 pScanner = mkScanner <$> pScannerID <*> pBeacons
   where
     mkScanner :: Int -> [Point] -> Scanner
-    mkScanner sId beacons = Scanner { scannerId      = sId
-                                    , beacons        = beacons
-                                    , transformation = nullT
-                                    , signature      = sign beacons
+    mkScanner sId beacons = Scanner { scannerId = sId
+                                    , beacons   = beacons
+                                    , rotation  = nullT
+                                    , signature = sign beacons
                                     }
 
 
