c0097dd43e35934d2c5f7049e3a1f1bc713a3611
Pedro Rodrigues <csixteen@protonmail.com>
2022-01-10 08:26:23 +0100

    Day 21 - part 2 (WIP)

--- 2021/Haskell/src/AOC/Day21.hs
+++ 2021/Haskell/src/AOC/Day21.hs
@@ -1,6 +1,9 @@
 module AOC.Day21 where
 
+import Control.Applicative
+import Data.Ix
 import Data.List
+import qualified Data.Map.Strict as M
 import Data.Maybe
 
 
@@ -8,7 +11,8 @@ solve :: IO (Int,Int)
 solve =
   do let board = mkBoard 3 7
          part1 = play board
-     return (part1,1)
+         part2 = (uncurry max) $ universes board
+     return (part1,part2)
 
 
 -- ---------------------------
@@ -25,7 +29,7 @@ data Player = Player Position Score
 
 
 data PlayerT = One | Two
-  deriving (Eq,Show)
+  deriving (Eq, Show)
 
 
 data Board = Board
@@ -38,6 +42,8 @@ data Board = Board
 
 
 type State = (Int, Board)
+type Cache = M.Map String (Int,Int)
+type Wins  = (Int,Int)
 
 
 mkBoard :: Position -> Position -> Board
@@ -48,10 +54,71 @@ mkBoard p1 p2 = Board { player1 = Player p1 0
                       }
 
 
+boardToStr :: Board -> String
+boardToStr Board{..} = concat $ intersperse "-" $ [show player1, show player2, show turn]
+
+
 -- ---------------------------
 --     Solvers and helpers
 -- ---------------------------
 
+
+-- Part 2
+
+universes :: Board -> Wins
+universes b = fst $ universes' b M.empty
+
+
+universes' :: Board -> Cache -> (Wins, Cache)
+universes' b cache = case cache M.!? (boardToStr b) of
+  Just v  -> (v, cache)
+  Nothing -> universes'' b cache
+
+
+universes'' :: Board -> Cache -> (Wins, Cache)
+universes'' b cache = fromJust $
+  (wins b cache) <|> (multiverse b cache)
+
+
+multiverse :: Board -> Cache -> Maybe (Wins, Cache)
+multiverse board cache = Just $ foldl' (mVerse board) ((0,0), cache) choices
+  where
+    mVerse :: Board -> (Wins,Cache) -> (Int,Int) -> (Wins,Cache)
+    mVerse _board ((a,b), _cache) (c,n) =
+      let
+        ((a',b'), cache') = universes' (updateBoard _board c) _cache
+        w = (a + a' * n, b + b' * n)
+        cache'' = M.insert (boardToStr board) w cache'
+      in
+        (w, cache'')
+
+
+wins :: Board -> Cache -> Maybe (Wins,Cache)
+wins b@Board{..} cache =
+  case hasWinner (0, b) of
+    False -> Nothing
+    True  ->
+      let
+        Player _ sc1 = player1
+        Player _ sc2 = player2
+        w = (if sc1 > 20 then 1 else 0, if sc2 > 20 then 1 else 0)
+        cache' = M.insert (boardToStr b) w cache
+      in
+        Just (w, cache')
+
+
+choices :: [(Int,Int)]
+choices = M.assocs m
+  where
+    m = foldl' addChoice M.empty $ range ((1,1,1), (3,3,3))
+    addChoice acc choice = M.insertWith (+) (sum3 choice) 1 acc
+
+
+sum3 :: (Int,Int,Int) -> Int
+sum3 (a,b,c) = a + b + c
+
+
+-- Part 1
 
 play :: Board -> Int
 play b = sc * (tosses b')
@@ -73,33 +140,36 @@ hasWinner (_, Board{..}) = isWinner player1 || isWinner player2
 
 
 step :: State -> State
-step (die, b@Board{..}) = (die', board')
-  where
-    (ds, die') = rollDie die
-    board' = case turn of
-               One -> updatePlayer1 ds b
-               Two -> updatePlayer2 ds b
+step (die, b) = (die', updateBoard b $ sum ds)
+  where (ds, die') = rollDie die
 
 
-updatePlayer1 :: [Int] -> Board -> Board
-updatePlayer1 ds Board{..} = Board { player1 = updatePlayer player1 ds
-                                   , turn    = Two
-                                   , tosses  = tosses + 3
-                                   , ..}
+updateBoard :: Board -> Int -> Board
+updateBoard b@Board{..} s =
+  case turn of
+    One -> updatePlayer1 s b
+    Two -> updatePlayer2 s b
 
 
-updatePlayer2 :: [Int] -> Board -> Board
-updatePlayer2 ds Board{..} = Board { player2 = updatePlayer player2 ds
-                                   , turn    = One
-                                   , tosses  = tosses + 3
-                                   , ..}
+updatePlayer1 :: Int -> Board -> Board
+updatePlayer1 s Board{..} = Board { player1 = updatePlayer player1 s
+                                  , turn    = Two
+                                  , tosses  = tosses + 3
+                                  , ..}
 
 
-updatePlayer :: Player -> [Int] -> Player
-updatePlayer (Player pos sc) ds = Player pos' sc'
+updatePlayer2 :: Int -> Board -> Board
+updatePlayer2 s Board{..} = Board { player2 = updatePlayer player2 s
+                                  , turn    = One
+                                  , tosses  = tosses + 3
+                                  , ..}
+
+
+updatePlayer :: Player -> Int -> Player
+updatePlayer (Player pos sc) s = Player pos' sc'
   where
     sc'  = sc + pos'
-    pos' = (pos + (sum ds)) `_mod` 10
+    pos' = (pos + s) `_mod` 10
 
 
 rollDie :: Int -> ([Int], Int)
