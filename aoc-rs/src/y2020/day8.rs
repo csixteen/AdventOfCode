0f5d0b7cee1dfefa502cd8e354944eab191f043a
Pedro Rodrigues <csixteen@protonmail.com>
2022-09-24 20:42:21 +0200

    Migrates Day 8 (2020)

--- aoc-rs/src/y2020/day8.rs
+++ aoc-rs/src/y2020/day8.rs
@@ -1,33 +1,9 @@
-// MIT License
-//
-// Copyright (c) 2020 Pedro Rodrigues
-//
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included in all
-// copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-// SOFTWARE.
-
-// https://adventofcode.com/2020/day/8
-
 #![allow(non_snake_case)]
 
 use std::collections::HashSet;
 use std::str::FromStr;
 
-use aoc::fs::get_file_contents;
+use aoc::Solver;
 
 
 #[derive(Clone, Debug, PartialEq)]
@@ -105,7 +81,7 @@ fn fix_and_execute(code: &Vec<(Instruction, i32)>) -> i32 {
     unreachable!();
 }
 
-fn parse_code(lines: Vec<String>) -> Vec<(Instruction, i32)> {
+fn parse_code(lines: &Vec<&str>) -> Vec<(Instruction, i32)> {
     lines
         .iter()
         .map(|line| {
@@ -122,15 +98,22 @@ fn parse_code(lines: Vec<String>) -> Vec<(Instruction, i32)> {
         .collect()
 }
 
-fn main() -> std::io::Result<()> {
-    let lines = get_file_contents("data/input.txt")?;
-    let code = parse_code(lines);
-    let mut p = Program::new(&code);
+pub struct Solution;
 
-    println!("Day 8 / Part 1: {:?}", p.execute());
-    println!("Day 8 / Part 2: {}", fix_and_execute(&code));
+impl Solver for Solution {
+    fn part1(&self, input: &Vec<&str>) -> String {
+        let code = parse_code(input);
+        let mut p = Program::new(&code);
+        match p.execute() {
+            Ok(v) => v.to_string(),
+            Err(v) => v.to_string(),
+        }
+    }
 
-    Ok(())
+    fn part2(&self, input: &Vec<&str>) -> String {
+        let code = parse_code(input);
+        fix_and_execute(&code).to_string()
+    }
 }
 
 #[cfg(test)]
